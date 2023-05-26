8f5392a7c42c73399d77796b2b73be84e9350a03
harsssh <66548698+harsssh@users.noreply.github.com>
2023-05-26 22:30:30 +0900

    update

--- .config/nvim/after/plugin/barbar.rc.lua
+++ .config/nvim/after/plugin/barbar.rc.lua
@@ -101,7 +101,7 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 -- Move to previous/next
-map('n', '<A-j>', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<A-k>', '<Cmd>BufferNext<CR>', opts)
+map('n', '<C-q>', '<Cmd>BufferPrevious<CR>', opts)
+map('n', '<C-w>', '<Cmd>BufferNext<CR>', opts)
 -- Close buffer
 map('n', '<leader>e', '<Cmd>BufferClose<CR>', opts)
