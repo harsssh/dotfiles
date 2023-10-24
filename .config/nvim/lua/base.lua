54787aebdafc493b02a1ee63cbea0b87b32baebf
harsh <66548698+harsssh@users.noreply.github.com>
2023-10-25 02:48:31 +0900

    diagnostic icon

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -59,7 +59,7 @@ vim.opt.backup = false   -- Disable backup
 vim.opt.swapfile = false -- Disable swap file
 
 -- LSP
-vim.fn.sign_define("DiagnosticSignError", { text = "", texthl = "DiagnosticSignError" })
-vim.fn.sign_define("DiagnosticSignWarn", { text = "", texthl = "DiagnosticSignWarn" })
-vim.fn.sign_define("DiagnosticSignInfo", { text = "", texthl = "DiagnosticSignInfo" })
-vim.fn.sign_define("DiagnosticSignHint", { text = "", texthl = "DiagnosticSignHint" })
+vim.fn.sign_define("DiagnosticSignError", { text = "󰜋", texthl = "DiagnosticSignError" })
+vim.fn.sign_define("DiagnosticSignWarn", { text = "󰜋", texthl = "DiagnosticSignWarn" })
+vim.fn.sign_define("DiagnosticSignInfo", { text = "󰜋", texthl = "DiagnosticSignInfo" })
+vim.fn.sign_define("DiagnosticSignHint", { text = "󰜋", texthl = "DiagnosticSignHint" })
