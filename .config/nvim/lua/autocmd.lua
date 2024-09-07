234c21e89e1e52ffdf0e0dd0f477a46573a58ade
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-09-08 05:27:37 +0900

    last position

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -24,3 +24,13 @@ vim.api.nvim_create_autocmd("TermOpen", {
     vim.opt_local.signcolumn = "no"
   end
 })
+
+-- カーソル位置を保存
+vim.api.nvim_create_autocmd("BufReadPost", {
+  pattern = "*",
+  callback = function()
+    if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+      vim.cmd('normal! g`"')
+    end
+  end
+})
