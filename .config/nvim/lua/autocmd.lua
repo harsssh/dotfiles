33a16117aa1433ad74f8e7bae4705c6d6d460c8f
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-09-08 06:17:22 +0900

    使ってないコード

--- .config/nvim/lua/autocmd.lua
+++ .config/nvim/lua/autocmd.lua
@@ -1,16 +1,3 @@
--- format on save
--- vim.api.nvim_create_autocmd("LspAttach", {
---   group = vim.api.nvim_create_augroup("lsp", { clear = true }),
---   callback = function(args)
---     vim.api.nvim_create_autocmd("BufWritePre", {
---       buffer = args.buf,
---       callback = function()
---         vim.lsp.buf.format {async = false, id = args.data.client_id }
---       end,
---     })
---   end
--- })
-
 vim.api.nvim_create_autocmd("TermOpen", {
   pattern = "*",
   command = "startinsert",
