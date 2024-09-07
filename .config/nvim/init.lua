6371f8afb33d29bd1d15b7a2a9182a8c40b416d4
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-09-08 08:37:23 +0900

    disable rtp plugins

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,30 @@
 if vim.g.vscode then
-    require("base")
-    require("keymap")
+  require("base")
+  require("keymap")
 else
-    require("init_lazy")
-    require("base")
-    require("keymap")
-    require("autocmd")
-    require("diagnostic")
+  require("init_lazy")
+  require("base")
+  require("keymap")
+  require("autocmd")
+  require("diagnostic")
 
-    require("lazy").setup("plugins", {
-      defaults = {
-        lazy = true,
-      },
-    })
+  require("lazy").setup("plugins", {
+    defaults = {
+      lazy = true,
+    },
+    performance = {
+      rtp = {
+        disabled_plugins = {
+          "gzip",
+          "matchit",
+          "matchparen",
+          "netrwPlugin",
+          "tarPlugin",
+          "tohtml",
+          "tutor",
+          "zipPlugin",
+        },
+      }
+    }
+  })
 end
