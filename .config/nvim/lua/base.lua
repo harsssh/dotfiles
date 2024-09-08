3032499f74aed90fe52e8a8dab0cd9b11c6e6567
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2024-09-08 16:30:00 +0900

    base: scrolloff

--- .config/nvim/lua/base.lua
+++ .config/nvim/lua/base.lua
@@ -1,6 +1,7 @@
 vim.g.mapleader = ' '
 
 vim.opt.syntax = "on"
+
 vim.wo.number = true
 vim.wo.relativenumber = true
 vim.opt.showtabline = 2
@@ -8,14 +9,15 @@ vim.opt.signcolumn = 'yes'
 vim.opt.termguicolors = true
 vim.opt.laststatus = 3
 vim.opt.cursorline = true
+vim.opt.pumheight = 12
+vim.opt.scrolloff = 4
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 vim.opt.timeoutlen = 300
-
-vim.opt.pumheight = 12
+vim.opt.updatetime = 300
 
 vim.opt.smartindent = true
 vim.opt.breakindent = true
