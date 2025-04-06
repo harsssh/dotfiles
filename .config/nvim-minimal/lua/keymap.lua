1d5d3819e0befac59da64498098a4cbb0a45cb42
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-04-07 08:53:58 +0900

    keymap

---
+++ .config/nvim-minimal/lua/keymap.lua
@@ -0,0 +1,56 @@
+local map = vim.keymap.set
+local silent = { silent = true }
+
+-- General
+map('i', 'jj', '<ESC>')
+map('n', '<ESC><ESC>', ':noh<CR>')
+map('n', '<leader>n', ':noh<CR>')
+
+-- UI
+map('n', 'rn', function() vim.wo.relativenumber = not vim.wo.relativenumber end, silent)
+
+-- Move
+map('n', 'j', 'gj', silent)
+map('v', 'j', 'gj', silent)
+map('n', 'k', 'gk', silent)
+map('v', 'k', 'gk', silent)
+map('n', 'H', '0', silent)
+map('v', 'H', '0', silent)
+map('n', 'L', '$', silent)
+map('v', 'L', '$', silent) -- Window
+map('n', 'sh', '<C-w>h')
+map('n', 'sj', '<C-w>j')
+map('n', 'sk', '<C-w>k')
+map('n', 'sl', '<C-w>l')
+map('n', 'sH', '<C-w>H')
+map('n', 'sJ', '<C-w>J')
+map('n', 'sK', '<C-w>K')
+map('n', 'sL', '<C-w>L')
+map('n', 'ss', ':split<CR><C-w>w')
+map('n', 'sv', ':vsplit<CR><C-w>w')
+
+-- Edit
+map('n', '+', '<C-a>')
+map('n', '-', '<C-x>')
+
+-- Yank
+map('n', 'Y', 'y$')
+
+-- Paste
+map('n', 'p', ']p')
+map('n', 'P', ']P')
+
+-- Command
+for _, cmd in ipairs({ 'q', 'w', 'qa', 'wq', 'wqa' }) do
+  vim.api.nvim_create_user_command(cmd:upper(), cmd, {})
+  if #cmd > 2 then
+    vim.api.nvim_create_user_command(cmd:sub(1, 1):upper() .. cmd:sub(2), cmd, {})
+  end
+end
+
+-- Terminal
+map('n', 'tt', "<cmd>terminal<CR>", silent)
+map('n', 'ts', "<cmd>belowright 15 new<CR><cmd>terminal<CR>", silent)
+map('n', 'tv', "<cmd>vertical belowright new<CR><cmd>terminal<CR>", silent)
+map('t', '<ESC>', [[<C-\><C-n>]], silent)
+
