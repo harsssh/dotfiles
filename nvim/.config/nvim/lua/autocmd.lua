0c992222a285a85298ae23cde85111fdbf9102b0
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-04-25 01:06:13 +0900

    refactor autocmd

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -37,3 +37,27 @@ vim.api.nvim_create_autocmd("BufReadPost", {
     end
   end
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local bufnr = args.buf
+    local opts = { buffer = bufnr }
+
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+    -- vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
+    vim.keymap.set('n', 'ge', vim.diagnostic.open_float, opts)
+    vim.keymap.set('n', 'g]', function()
+      vim.diagnostic.jump({ forward = true })
+    end, opts)
+    vim.keymap.set('n', 'g[', function()
+      vim.diagnostic.jump({ forward = false })
+    end, opts)
+    vim.keymap.set('n', 'gf', vim.lsp.buf.format, opts)
+    vim.keymap.set('n', 'gn', vim.lsp.buf.rename, opts)
+    vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, opts)
+  end
+})
--- nvim/.config/nvim/lua/diagnostic.lua
+++ nvim/.config/nvim/lua/diagnostic.lua
@@ -8,25 +8,5 @@ vim.diagnostic.config({
   underline = true,
 })
 
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local bufnr = args.buf
-    local opts = { buffer = bufnr }
-
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    -- vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', 'ge', vim.diagnostic.open_float, opts)
-    vim.keymap.set('n', 'g]', vim.diagnostic.goto_next, opts)
-    vim.keymap.set('n', 'g[', vim.diagnostic.goto_prev, opts)
-    vim.keymap.set('n', 'gf', vim.lsp.buf.format, opts)
-    vim.keymap.set('n', 'gn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, opts)
-  end
-})
-
 vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { undercurl = true, sp = "Red" })
 vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn", { undercurl = true, sp = "Orange" })
