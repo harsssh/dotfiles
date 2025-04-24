35b4e2d53cf8d9e402162e45527cda0259572e39
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-04-24 23:18:03 +0900

    rm finalize.zsh

--- zsh/.zsh/path.zsh
+++ zsh/.zsh/path.zsh
@@ -1,14 +1,33 @@
+typeset -U path PATH
+
 PATH=/opt/homebrew/opt/coreutils/bin:$PATH
 PATH=/opt/homebrew/opt/openssl@3/bin:$PATH
 PATH=~/.local/bin:$PATH
 PATH=/opt/homebrew/opt/llvm/bin:$PATH
+PATH="$PATH:/Applications/WezTerm.app/Contents/MacOS"
 
 export BUN_INSTALL="$HOME/.bun"
 PATH="$BUN_INSTALL/bin:$PATH"
 
-PATH="$PATH:/Applications/WezTerm.app/Contents/MacOS"
+# fzf
+if command -v fzf >/dev/null 2>&1; then
+  eval "$(fzf --zsh)"
+fi
+# starship
+if command -v starship >/dev/null 2>&1; then
+  eval "$(starship init zsh)"
+fi
+# zoxide
+if command -v zoxide >/dev/null 2>&1; then
+  eval "$(zoxide init zsh)"
+fi
+# mise
+if command -v mise >/dev/null 2>&1; then
+    eval "$(mise activate zsh)"
+fi
 
-# bun
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
+[ -f ~/.cargo/env ] && source ~/.cargo/env
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+[ -s ~/.bun/_bun ] && source ~/.bun/_bun
+[ -f ~/.ghcup/env ] && source ~/.ghcup/env
 
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,10 +1,8 @@
 source ~/.zsh/env.zsh
 
 source ~/.zsh/options.zsh
-source ~/.zsh/path.zsh
-source ~/.zsh/aliases.zsh
-source ~/.zsh/functions.zsh
 source ~/.zsh/zinit.zsh
-
-source ~/.zsh/finalize.zsh
+source ~/.zsh/functions.zsh
+source ~/.zsh/aliases.zsh
+source ~/.zsh/path.zsh
 
