e5add191cac95cda8b3b6d3a2ee4f47be8fa7f17
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-04-25 00:50:14 +0900

    use vim.lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -11,6 +11,19 @@ return {
       "williamboman/mason.nvim",
       "neovim/nvim-lspconfig",
     },
-    config = function() require("plugins/config/lspconfig").setup() end,
+    config = function ()
+      require("mason").setup()
+
+      local mason_lspconfig = require('mason-lspconfig')
+      mason_lspconfig.setup {
+        automatic_installation = true,
+        ensure_installed = {'ts_ls', 'lua_ls', 'hls', 'gopls'},
+      }
+
+      vim.lsp.config('*', {
+        capabilities = require('cmp_nvim_lsp').default_capabilities(),
+      })
+      vim.lsp.enable(mason_lspconfig.get_installed_servers())
+    end
   },
 }
