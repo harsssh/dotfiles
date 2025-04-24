b4038ac8f1aa6db5325810efee15d5d6947f1b2f
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-04-24 23:06:01 +0900

    wezterm / remove flutter plugin

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,4 +4,17 @@ local config = wezterm.config_builder()
 config.font = wezterm.font 'Monaspace Argon'
 config.color_scheme = 'GitHub Dark'
 
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.use_ime = true
+
+-- デフォルトは option + enter
+config.keys = {
+  {
+    key = 'n',
+    mods = 'SHIFT|CTRL',
+    action = wezterm.action.ToggleFullScreen,
+  },
+}
+
 return config
