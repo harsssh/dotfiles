d935ccb6fd94c062b6c8c6fce31963624ecac1ef
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-04-25 10:19:45 +0900

    update wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,20 +1,35 @@
 local wezterm = require 'wezterm'
+local act = wezterm.action
+
 local config = wezterm.config_builder()
 
 config.font = wezterm.font 'Monaspace Argon'
+config.font_size = 14
 config.color_scheme = 'GitHub Dark'
 
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
 config.use_ime = true
 
--- デフォルトは option + enter
 config.keys = {
+  -- デフォルトは option + enter
   {
     key = 'n',
     mods = 'SHIFT|CTRL',
-    action = wezterm.action.ToggleFullScreen,
+    action = act.ToggleFullScreen,
   },
+  -- ⌘ + d で右方向にペイン分割
+  { key = 'd', mods = 'CMD',       action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+  -- ⌘ + Shift + d で下方向にペイン分割
+  { key = 'd', mods = 'CMD|SHIFT', action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
+  -- ⌘ + w でペインを閉じる
+  { key = 'w', mods = 'CMD',       action = act.CloseCurrentPane { confirm = true } },
+  -- ⌘ + [ / ] でペイン間の移動
+  { key = '[', mods = 'CMD',       action = act.ActivatePaneDirection 'Left' },
+  { key = ']', mods = 'CMD',       action = act.ActivatePaneDirection 'Right' },
+  -- ⌘ + Shift + [ / ] でタブ間の移動
+  { key = '[', mods = 'CMD|SHIFT', action = act.ActivateTabRelative(-1) },
+  { key = ']', mods = 'CMD|SHIFT', action = act.ActivateTabRelative(1) },
 }
 
 return config
