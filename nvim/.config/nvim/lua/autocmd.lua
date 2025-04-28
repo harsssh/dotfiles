5f50a77de8167d740fe236ccbe27d82b29950a9a
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-04-28 18:31:33 +0900

    improve autocmd

--- nvim/.config/nvim/lua/autocmd.lua
+++ nvim/.config/nvim/lua/autocmd.lua
@@ -1,63 +1,73 @@
--- terminal を開いたら insert mode に入る。行番号などは表示しない。
--- command, callback は一度に設定できないので分ける
-vim.api.nvim_create_autocmd("TermOpen", {
-  pattern = "*",
-  command = "startinsert",
-})
-vim.api.nvim_create_autocmd("TermOpen", {
-  pattern = "*",
+local aug = vim.api.nvim_create_augroup
+local autocmd = vim.api.nvim_create_autocmd
+
+-- terminal を開いたら insert mode に入る (行番号などは表示しない)
+aug('MyTerminal', { clear = true })
+autocmd('TermOpen', {
+  group    = 'MyTerminal',
+  pattern  = '*',
   callback = function()
-    vim.opt_local.number = false
+    vim.cmd('startinsert')
+    vim.opt_local.number         = false
     vim.opt_local.relativenumber = false
-    vim.opt_local.signcolumn = "no"
-  end
-})
-
-
--- help などで余計な column を表示させない
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "help", "man", "qf", "lspinfo", "checkhealth" },
-  callback = function()
-    vim.opt_local.signcolumn = "no"
+    vim.opt_local.signcolumn     = 'no'
   end,
 })
 
--- 外部からファイルを変更されたら反映する
-vim.api.nvim_create_autocmd({ "WinEnter", "FocusGained", "BufEnter" }, {
-  pattern = "*",
-  command = "checktime",
+-- 外部から変更のあったファイルを自動リロード
+aug('AutoReload', { clear = true })
+autocmd({ 'WinEnter', 'FocusGained', 'BufEnter' }, {
+  group   = 'AutoReload',
+  pattern = '*',
+  command = 'checktime',
 })
 
--- カーソル位置を保存
-vim.api.nvim_create_autocmd("BufReadPost", {
-  pattern = "*",
+-- Help 系で signcolumn を非表示
+aug('HelpDisplay', { clear = true })
+autocmd('FileType', {
+  group    = 'HelpDisplay',
+  pattern  = { 'help', 'man', 'qf', 'lspinfo', 'checkhealth' },
   callback = function()
-    if vim.fn.line("'\"") > 1 and vim.fn.line("'\"") <= vim.fn.line("$") then
+    vim.opt_local.signcolumn = 'no'
+  end,
+})
+
+
+-- カーソル位置保存
+aug('RestoreCursor', { clear = true })
+autocmd('BufReadPost', {
+  group = 'RestoreCursor',
+  pattern = '*',
+  callback = function()
+    local mark = vim.fn.line("'\"")
+    if mark > 1 and mark <= vim.fn.line("$") then
       vim.cmd('normal! g`"')
     end
-  end
+  end,
 })
 
-vim.api.nvim_create_autocmd("LspAttach", {
+-- LSP アタッチ時のキーマップ
+aug('LspKeymaps', { clear = true })
+autocmd('LspAttach', {
+  group = 'LspKeymaps',
   callback = function(args)
     local bufnr = args.buf
-    local opts = { buffer = bufnr }
-
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-    -- vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, opts)
-    vim.keymap.set('n', 'ge', vim.diagnostic.open_float, opts)
-    vim.keymap.set('n', 'g]', function()
+    local opts  = { buffer = bufnr, silent = true }
+    local map   = vim.keymap.set
+    map('n', 'K', vim.lsp.buf.hover, opts)
+    map('n', 'gr', vim.lsp.buf.references, opts)
+    map('n', 'gd', vim.lsp.buf.definition, opts)
+    map('n', 'gD', vim.lsp.buf.declaration, opts)
+    map('n', 'gi', vim.lsp.buf.implementation, opts)
+    map('n', 'ge', vim.diagnostic.open_float, opts)
+    map('n', 'g]', function()
       vim.diagnostic.jump({ count = 1, forward = true })
     end, opts)
-    vim.keymap.set('n', 'g[', function()
+    map('n', 'g[', function()
       vim.diagnostic.jump({ count = 1, forward = false })
     end, opts)
-    vim.keymap.set('n', 'gf', vim.lsp.buf.format, opts)
-    vim.keymap.set('n', 'gn', vim.lsp.buf.rename, opts)
-    vim.keymap.set('n', 'ga', vim.lsp.buf.code_action, opts)
-  end
+    map('n', 'gf', vim.lsp.buf.format, opts)
+    map('n', 'gn', vim.lsp.buf.rename, opts)
+    map('n', 'ga', vim.lsp.buf.code_action, opts)
+  end,
 })
