530c18f188f8be256c1333cfb91eeac8fd8f17c3
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-11-18 16:46:23 +0900

    alias

--- zsh/.zsh/user.zsh
+++ zsh/.zsh/user.zsh
@@ -75,6 +75,7 @@ alias tmux='tmux -2'  # Force 256 color support
 alias ta='tmux attach -t'
 alias tad='tmux attach -d -t'
 alias ts='tmux new-session -s'
+alias tsd='tmux new-session -d -s'
 alias tl='tmux list-sessions'
 alias tksv='tmux kill-server'
 alias tkss='tmux kill-session -t'
