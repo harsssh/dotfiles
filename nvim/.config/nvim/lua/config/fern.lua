9f2f839b5cf672ac90dd49287b69d9256788ae7b
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2025-11-19 16:34:31 +0900

    fern 内で vim-tmux-navigator を使用してウィンドウ移動を可能にする
    fern バッファ内で Ctrl+hjkl を使ったウィンドウ移動ができなかった問題を修正。
    vim-tmux-navigator のコマンドを直接使用することで、nvim ウィンドウ間の移動だけでなく、
    tmux pane への移動も可能になった。

--- nvim/.config/nvim/lua/config/fern.lua
+++ nvim/.config/nvim/lua/config/fern.lua
@@ -14,6 +14,13 @@ M.setup = function()
       vim.keymap.set('n', 's', '<Nop>', { buffer = true, noremap = true, silent = true })
       -- ファイルの場合は何もしない
       vim.keymap.set('n', 'l', '<Plug>(fern-action-expand)', { buffer = true, noremap = true, silent = true })
+
+      -- fern 内でも vim-tmux-navigator を使ってウィンドウ移動を可能にする
+      -- tmux 環境では tmux pane への移動も可能
+      vim.keymap.set('n', '<C-h>', '<cmd>TmuxNavigateLeft<cr>', { buffer = true, noremap = true, silent = true })
+      vim.keymap.set('n', '<C-l>', '<cmd>TmuxNavigateRight<cr>', { buffer = true, noremap = true, silent = true })
+      vim.keymap.set('n', '<C-j>', '<cmd>TmuxNavigateDown<cr>', { buffer = true, noremap = true, silent = true })
+      vim.keymap.set('n', '<C-k>', '<cmd>TmuxNavigateUp<cr>', { buffer = true, noremap = true, silent = true })
     end,
   })
 end
