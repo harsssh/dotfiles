7f54b1195ae8f71cb649951ebd8bea50e6e67345
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2026-01-27 06:26:24 +0900

    use cachix

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -10,6 +10,13 @@ in
   users.users.${username}.home = homeDirectory;
   nix.enable = false;
 
+  environment.etc."nix/nix.custom.conf".text = ''
+    extra-substituters = https://nix-community.cachix.org
+    extra-trusted-public-keys = nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+    http-connections = 50
+    narinfo-cache-positive-ttl = 86400
+  '';
+
   home-manager.useGlobalPkgs = true;
   home-manager.useUserPackages = true;
   home-manager.backupFileExtension = "backup";
