3b53e60abb91af18f46581bdb796f757baef56f3
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2026-01-29 20:25:05 +0900

    update

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -11,8 +11,8 @@ in
   nix.enable = false;
 
   environment.etc."nix/nix.custom.conf".text = ''
-    extra-substituters = https://harsssh.cachix.org https://nix-community.cachix.org
-    extra-trusted-public-keys = harsssh.cachix.org-1:65C0xZ93n9w8nRj7Sf5sOHHTcVeWDEEO2N6ckeVDRJM= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=
+    extra-substituters = https://harsssh.cachix.org https://nix-community.cachix.org https://devenv.cachix.org
+    extra-trusted-public-keys = harsssh.cachix.org-1:65C0xZ93n9w8nRj7Sf5sOHHTcVeWDEEO2N6ckeVDRJM= nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=
     http-connections = 50
     narinfo-cache-positive-ttl = 86400
   '';
