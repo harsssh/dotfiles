d5fb8877e247e7e61f7b75fa90bb0cab4b612d63
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2026-05-13 20:16:26 +0900

    update snacks setting

--- modules/home/neovim/plugins/snacks.nix
+++ modules/home/neovim/plugins/snacks.nix
@@ -21,8 +21,8 @@ in
           scope = {
             enabled = true;
             char = "▏";
-            animate.enabled = false;
           };
+          animate.enabled = false;
         };
         words = { enabled = true; };
         rename = { enabled = true; };
@@ -48,32 +48,52 @@ in
           enabled = true;
           replace_netrw = true;
         };
+        picker.sources.explorer.layout.layout.position = "right";
       };
     };
 
     keymaps = [
-      # Picker
-      { mode = "n"; key = "<leader>ff"; action = mkRaw "function() Snacks.picker.files() end"; options.desc = "Picker: Find Files"; }
-      { mode = "n"; key = "<leader>fg"; action = mkRaw "function() Snacks.picker.grep() end"; options.desc = "Picker: Live Grep"; }
-      { mode = "n"; key = "<leader>fb"; action = mkRaw "function() Snacks.picker.lines() end"; options.desc = "Picker: Buffer Lines"; }
-      { mode = "n"; key = "<leader>fe"; action = mkRaw "function() Snacks.picker.recent() end"; options.desc = "Picker: Recent Files"; }
-      { mode = "n"; key = "<leader>fB"; action = mkRaw "function() Snacks.picker.buffers() end"; options.desc = "Picker: Buffers"; }
-      { mode = "n"; key = "<leader>gs"; action = mkRaw "function() Snacks.picker.git_status() end"; options.desc = "Picker: Git Status"; }
-      {
-        mode = "n";
-        key = "<leader>fs";
-        action = mkRaw ''
-          function()
-            Snacks.picker.lsp_workspace_symbols({
-              filter = { default = { "Class", "Function", "Method", "Interface", "Struct", "Module", "Constant", "Enum" } },
-            })
-          end
-        '';
-        options.desc = "Picker: Workspace Symbols";
-      }
+      # Top Pickers & Explorer
+      { mode = "n"; key = "<leader><space>"; action = mkRaw "function() Snacks.picker.smart() end"; options.desc = "Smart Find Files"; }
+      { mode = "n"; key = "<leader>,"; action = mkRaw "function() Snacks.picker.buffers() end"; options.desc = "Buffers"; }
+      { mode = "n"; key = "<leader>/"; action = mkRaw "function() Snacks.picker.grep() end"; options.desc = "Grep"; }
+      { mode = "n"; key = "<leader>e"; action = mkRaw "function() Snacks.explorer() end"; options.desc = "File Explorer"; }
+      { mode = "n"; key = "-"; action = mkRaw "function() Snacks.explorer() end"; options.desc = "File Explorer"; }
 
-      # Explorer (fern の `-` を置き換え)
-      { mode = "n"; key = "-"; action = mkRaw "function() Snacks.explorer() end"; options.desc = "Explorer"; }
+      # Find
+      { mode = "n"; key = "<leader>fb"; action = mkRaw "function() Snacks.picker.buffers() end"; options.desc = "Buffers"; }
+      { mode = "n"; key = "<leader>fc"; action = mkRaw ''function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end''; options.desc = "Find Config File"; }
+      { mode = "n"; key = "<leader>ff"; action = mkRaw "function() Snacks.picker.files() end"; options.desc = "Find Files"; }
+      { mode = "n"; key = "<leader>fg"; action = mkRaw "function() Snacks.picker.git_files() end"; options.desc = "Find Git Files"; }
+      { mode = "n"; key = "<leader>fr"; action = mkRaw "function() Snacks.picker.recent() end"; options.desc = "Recent"; }
+
+      # Git
+      { mode = "n"; key = "<leader>gl"; action = mkRaw "function() Snacks.picker.git_log() end"; options.desc = "Git Log"; }
+      { mode = "n"; key = "<leader>gs"; action = mkRaw "function() Snacks.picker.git_status() end"; options.desc = "Git Status"; }
+      { mode = "n"; key = "<leader>gd"; action = mkRaw "function() Snacks.picker.git_diff() end"; options.desc = "Git Diff (Hunks)"; }
+      { mode = "n"; key = "<leader>gf"; action = mkRaw "function() Snacks.picker.git_log_file() end"; options.desc = "Git Log File"; }
+
+      # Grep
+      { mode = "n"; key = "<leader>sb"; action = mkRaw "function() Snacks.picker.lines() end"; options.desc = "Buffer Lines"; }
+      { mode = "n"; key = "<leader>sB"; action = mkRaw "function() Snacks.picker.grep_buffers() end"; options.desc = "Grep Open Buffers"; }
+      { mode = "n"; key = "<leader>sg"; action = mkRaw "function() Snacks.picker.grep() end"; options.desc = "Grep"; }
+
+      # Search
+      { mode = "n"; key = "<leader>sd"; action = mkRaw "function() Snacks.picker.diagnostics() end"; options.desc = "Diagnostics"; }
+      { mode = "n"; key = "<leader>sD"; action = mkRaw "function() Snacks.picker.diagnostics_buffer() end"; options.desc = "Buffer Diagnostics"; }
+      { mode = "n"; key = "<leader>sm"; action = mkRaw "function() Snacks.picker.marks() end"; options.desc = "Marks"; }
+      { mode = "n"; key = "<leader>sq"; action = mkRaw "function() Snacks.picker.qflist() end"; options.desc = "Quickfix List"; }
+
+      # LSP
+      { mode = "n"; key = "gd"; action = mkRaw "function() Snacks.picker.lsp_definitions() end"; options.desc = "Goto Definition"; }
+      { mode = "n"; key = "gD"; action = mkRaw "function() Snacks.picker.lsp_declarations() end"; options.desc = "Goto Declaration"; }
+      { mode = "n"; key = "gr"; action = mkRaw "function() Snacks.picker.lsp_references() end"; options = { desc = "References"; nowait = true; }; }
+      { mode = "n"; key = "gI"; action = mkRaw "function() Snacks.picker.lsp_implementations() end"; options.desc = "Goto Implementation"; }
+      { mode = "n"; key = "gy"; action = mkRaw "function() Snacks.picker.lsp_type_definitions() end"; options.desc = "Goto Type Definition"; }
+      { mode = "n"; key = "gai"; action = mkRaw "function() Snacks.picker.lsp_incoming_calls() end"; options.desc = "Calls Incoming"; }
+      { mode = "n"; key = "gao"; action = mkRaw "function() Snacks.picker.lsp_outgoing_calls() end"; options.desc = "Calls Outgoing"; }
+      { mode = "n"; key = "<leader>ss"; action = mkRaw "function() Snacks.picker.lsp_symbols() end"; options.desc = "LSP Symbols"; }
+      { mode = "n"; key = "<leader>sS"; action = mkRaw "function() Snacks.picker.lsp_workspace_symbols() end"; options.desc = "LSP Workspace Symbols"; }
 
       # LazyGit
       { mode = "n"; key = "<leader>lg"; action = mkRaw "function() Snacks.lazygit() end"; options.desc = "LazyGit"; }
@@ -81,9 +101,10 @@ in
       # Bufdelete (mini.bufremove の置き換え)
       { mode = "n"; key = "<leader>bd"; action = mkRaw "function() Snacks.bufdelete() end"; options.desc = "Delete Buffer"; }
       { mode = "n"; key = "<leader>bo"; action = mkRaw "function() Snacks.bufdelete.other() end"; options.desc = "Delete Other Buffers"; }
+      { mode = "n"; key = "<leader>ba"; action = mkRaw "function() Snacks.bufdelete.all() end"; options.desc = "Delete All Buffers"; }
 
       # Rename (LSP 連動のファイルリネーム)
-      { mode = "n"; key = "<leader>rf"; action = mkRaw "function() Snacks.rename.rename_file() end"; options.desc = "Rename File"; }
+      { mode = "n"; key = "R"; action = mkRaw "function() Snacks.rename.rename_file() end"; options.desc = "Rename File"; }
 
       # Words (LSP reference の前後ジャンプ)
       { mode = "n"; key = "]]"; action = mkRaw "function() Snacks.words.jump(vim.v.count1) end"; options.desc = "Next Reference"; }
