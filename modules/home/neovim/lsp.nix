24a85177b6ea149c0c1479fbdccf7dc5f0cd17fc
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2026-05-14 12:47:53 +0900

    ruby lsp のエラー回避

--- modules/home/neovim/lsp.nix
+++ modules/home/neovim/lsp.nix
@@ -1,3 +1,4 @@
+{ config, ... }:
 {
   programs.nixvim.plugins.lsp = {
     enable = true;
@@ -14,6 +15,10 @@
       };
       ruby_lsp = {
         enable = true;
+        # nix store は read-only なので GEM_HOME を書き込み可能なパスに向ける
+        extraOptions.cmd_env = {
+          GEM_HOME = "${config.home.homeDirectory}/.local/share/ruby-lsp-gems";
+        };
         onAttach.function = ''
           client.server_capabilities.documentFormattingProvider = false
           client.server_capabilities.documentRangeFormattingProvider = false
