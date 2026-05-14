b09ccb73a14671c3b1bbbbef4e457806888040b9
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2026-05-14 11:56:52 +0900

    効いてなさそうな設定を削除

--- modules/home/neovim/diagnostics.nix
+++ modules/home/neovim/diagnostics.nix
@@ -8,13 +8,5 @@
       underline = true;
       update_in_insert = false;
     };
-
-    # 下線を undercurl + 色付きに
-    extraConfigLuaPost = ''
-      vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { undercurl = true, sp = "#FF5555" })
-      vim.api.nvim_set_hl(0, "DiagnosticUnderlineWarn",  { undercurl = true, sp = "#FFAA00" })
-      vim.api.nvim_set_hl(0, "DiagnosticUnderlineInfo",  { undercurl = true, sp = "#61AFEF" })
-      vim.api.nvim_set_hl(0, "DiagnosticUnderlineHint",  { undercurl = true, sp = "#98C379" })
-    '';
   };
 }
