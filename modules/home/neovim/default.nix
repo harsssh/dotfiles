cb0781018582efb63710a783ac8185ce7506c7f7
Kentaro Mizuki <66548698+harsssh@users.noreply.github.com>
2026-08-18 14:20:09 +0900

    fix ruby lsp

--- modules/home/neovim/default.nix
+++ modules/home/neovim/default.nix
@@ -18,6 +18,10 @@
     vimAlias = true;
     performance.byteCompileLua.enable = true;
 
+    # Ruby provider は使っていない。有効だと nvim のラッパーが GEM_HOME を
+    # 自身の gem 環境へ上書きし、LSP など子プロセスの gem 解決を壊す
+    withRuby = false;
+
     colorschemes.everforest = {
       enable = true;
       settings = {
